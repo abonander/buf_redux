73bca994e9b737cfdac0ad6b46887d6afa492765
Austin Bonander <austin.bonander@gmail.com>
2016-02-07 03:11:00 -0800

    Another typo fix

--- src/tests.rs
+++ src/tests.rs
@@ -9,7 +9,7 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
-se std::io::prelude::*;
+use std::io::prelude::*;
 use std::io::{self, SeekFrom};
 use super::BufReader;
 
