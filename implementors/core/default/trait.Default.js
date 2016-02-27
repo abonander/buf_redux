3c951ae944e6ed4347f04049dad39a4151eeab8e
Austin Bonander <austin.bonander@gmail.com>
2016-02-26 23:51:50 -0800

    Update docs to 0.2.0

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['buf_redux'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='buf_redux/strategy/struct.IfEmpty.html' title='buf_redux::strategy::IfEmpty'>IfEmpty</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='buf_redux/strategy/struct.LessThan.html' title='buf_redux::strategy::LessThan'>LessThan</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='buf_redux/strategy/struct.AtEndLessThan1k.html' title='buf_redux::strategy::AtEndLessThan1k'>AtEndLessThan1k</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='buf_redux/strategy/struct.AtEndLessThan.html' title='buf_redux::strategy::AtEndLessThan'>AtEndLessThan</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='buf_redux/strategy/struct.NeverMove.html' title='buf_redux::strategy::NeverMove'>NeverMove</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- implementors/std/io/trait.BufRead.js
+++ implementors/std/io/trait.BufRead.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['buf_redux'] = ["impl&lt;R: <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.BufRead.html' title='std::io::BufRead'>BufRead</a> for <a class='struct' href='buf_redux/struct.BufReader.html' title='buf_redux::BufReader'>BufReader</a>&lt;R&gt;",];
+implementors['buf_redux'] = ["impl&lt;R: <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a>, Rs: <a class='trait' href='buf_redux/strategy/trait.ReadStrategy.html' title='buf_redux::strategy::ReadStrategy'>ReadStrategy</a>, Ms: <a class='trait' href='buf_redux/strategy/trait.MoveStrategy.html' title='buf_redux::strategy::MoveStrategy'>MoveStrategy</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.BufRead.html' title='std::io::BufRead'>BufRead</a> for <a class='struct' href='buf_redux/struct.BufReader.html' title='buf_redux::BufReader'>BufReader</a>&lt;R, Rs, Ms&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
