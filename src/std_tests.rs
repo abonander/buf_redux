373d6c1fe61f941a7cd5c44da67fd17840553eea
Austin Bonander <austin.bonander@gmail.com>
2018-05-07 01:26:22 -0700

    expand `test_min_buffered()`

--- src/std_tests.rs
+++ src/std_tests.rs
@@ -323,9 +323,12 @@ fn test_min_buffered() {
         .set_policy(MinBuffered(min_buffered));
 
     assert_eq!(reader.fill_buf().unwrap(), &[0, 1, 2, 3, 4, 5, 6, 7, 8][..]);
-    reader.consume(4);
+    reader.consume(2);
+    assert_eq!(reader.fill_buf().unwrap(), &[2, 3, 4, 5, 6, 7, 8][..]);
+    reader.consume(2);
     assert_eq!(reader.fill_buf().unwrap(), &[4, 5, 6, 7, 8][..]);
     reader.consume(4);
+    // normal reader wouldn't get more data until it was empty
     assert_eq!(reader.fill_buf().unwrap(), &[8, 9, 10, 11, 12, 13, 14, 15, 16][..]);
     reader.consume(8);
     assert_eq!(reader.fill_buf().unwrap(), &[16, 17, 18, 19]);
